c80b5a4f7c9ea998c653fa0645b79c90fd5289e9
Bas de Haas <W.B.deHaas@uu.nl>
2013-03-20 16:00:46 +0100

    added some evaluation code

--- SepHand.hs
+++ SepHand.hs
@@ -4,7 +4,7 @@ import ZMidi.Core         ( writeMidi )
 import ZMidiBasic
 import MidiCommonIO       ( readMidiScore, mapDir )
 
-import Data.List          ( intercalate, sort, groupBy )
+import Data.List          ( intercalate, sort, groupBy, genericLength, intersect )
 import Data.Function      ( on )
 import Control.Arrow      ( (***) )
 import System.Environment ( getArgs )
@@ -62,3 +62,39 @@ pickHigh :: [Timed ScoreEvent] -> ([Timed ScoreEvent],[Timed ScoreEvent])
 pickHigh [ ] = error "pickHigh: empty list"
 pickHigh [x] = ([],[x])
 pickHigh l   = let (h:t) = reverse . sort $ l in (t,[h])
+
+--------------------------------------------------------------------------------
+-- Evaluation
+--------------------------------------------------------------------------------
+
+-- | calculates the the triplet of Precision, Recall and F-Measure given
+-- a list of segmentable groundtruth items and test items.
+precRecF :: Eq a => [a] -> [a] -> PrecisionRecallFMeasure
+precRecF gt test = let inters    = genericLength (gt `intersect` test) 
+                       precision = inters / genericLength test
+                       recall    = inters / genericLength gt
+                       fMeasure  = (2 * precision * recall) / (precision + recall)
+                   in precision `seq` recall `seq` fMeasure 
+                                `seq` PRF precision recall fMeasure
+
+-- | Datatype for storing the triplet of Precision, Recall and F-Measure
+data PrecisionRecallFMeasure = PRF Double Double Double 
+
+instance Show PrecisionRecallFMeasure where
+  -- show (PRF p r f) = show p ++ '\t' : show r ++ '\t' : show f
+  show (PRF p r f) = intercalate "\t" . map show $ [p,r,f]
+  
+{-
+-- | a version of 'Data.List.intersect' that takes advantage of the fact that
+-- the two intersected lists are sorted ascendingly (it is approximatly 30 times
+-- as fast)
+sortIntersect :: Ord a => [a] -> [a] -> [a]
+sortIntersect [] _  = []
+sortIntersect _  [] = []
+sortIntersect (x:xs) ys  
+  | null rest      =     sortIntersect xs rest
+  | x == head rest = x : sortIntersect xs (tail rest)
+  | otherwise      =     sortIntersect xs rest
+      where rest = dropWhile (< x) ys
+ -}
+ 
