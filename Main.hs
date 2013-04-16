98a9e6093cc191169865ecc7b8e5528695c01caf
Bas de Haas <W.B.deHaas@uu.nl>
2013-04-16 17:50:27 +0200

    rewritten the cli

--- Main.hs
+++ Main.hs
@@ -1,23 +1,67 @@
+{-# OPTIONS_GHC -Wall #-}
 module Main where
 
 import ZMidi.Core         ( readMidi )
 import ZMidiBasic
 import MidiCommonIO       ( mapDirInDir, mapDir, readMidiScore )
 
-import System.Environment ( getArgs )
+-- import System.Environment ( getArgs )
 import Data.IntMap.Lazy   ( keys )
 
-main :: IO ()
-main = do arg <- getArgs
-          case arg of
-            ["-d", d] -> do putStrLn ("filepath\tTime Signatures\tKeys\t" 
-                                   ++ "Nr. Voices"
-                                   ++ "\tgcIOI divisor\tticks p. beat"
-                                   ++ "\tNr. Notes\tnr. div. durations\tdurations")
-                            mapDirInDir (mapDir showMidiStats) d
-            ["-f", f] -> doScore f
-            _         -> putStrLn "usage:  -f <filename> OR -d <directory>"
+-- other libraries
+import System.Console.ParseArgs
+import RTCParser          ( parseComp )
 
+data RagArgs = Mode| MidiDir | RTC | MidiFile deriving (Eq, Ord, Show)
+
+myArgs :: [Arg RagArgs]
+myArgs = [ 
+          Arg { argIndex = Mode,
+                 argAbbr  = Just 'm',
+                 argName  = Just "mode",
+                 argData  = argDataRequired "<rtc|stat>" ArgtypeString,
+                 argDesc  = "Mode of operation"
+               }
+         , Arg { argIndex = MidiDir,
+                 argAbbr  = Just 'd',
+                 argName  = Just "dir",
+                 argData  = argDataOptional "filepath" ArgtypeString,
+                 argDesc  = "Base directory containing the MIDI files"
+               }
+         , Arg { argIndex = RTC,
+                 argAbbr  = Just 'c',
+                 argName  = Just "rtc",
+                 argData  = argDataOptional "filpath" ArgtypeString,
+                 argDesc  = "The file containing the ragtime compendium"
+               }
+         , Arg { argIndex = MidiFile,
+                 argAbbr  = Just 'f',
+                 argName  = Just "file",
+                 argData  = argDataOptional "filepath" ArgtypeString,
+                 argDesc  = "test"
+               }
+         ] 
+
+main :: IO ()
+main = do args <- parseArgsIO ArgsComplete myArgs
+          case (getRequiredArg args Mode, fileOrDir args) of
+            ("stat", Left f ) -> doScore f
+            ("stat", Right d) -> mapDirInDir (mapDir showMidiStats) d
+            ("rtc" , Right d) -> do case getArg args RTC of
+                                      Just c  -> parseComp c
+                                      Nothing -> usageError args 
+                                                 "no compendium specified"
+            (m     , _      ) -> usageError args ("invalid mode: " ++ m )
+            
+            
+
+fileOrDir :: Args RagArgs -> Either FilePath FilePath
+fileOrDir args = case (getArg args MidiFile, getArg args MidiDir) of
+   (Just _, Just _) -> usageError args "found both a directory and file"
+   (Just f, _     ) -> Left f
+   (_     , Just d) -> Right d
+   (_     , _     ) -> usageError args "No directory or file specified"
+          
 
 -- | do stuff with a 'MidiScore' ...
 doScore :: FilePath -> IO ()
