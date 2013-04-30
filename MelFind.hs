0d022015bb6e8b97365cf79d28e09f2512680220
Bas de Haas <wbdehaas@xs4all.nl>
2013-04-30 15:56:38 +0200

    implemented dip detection, needs testing

--- MelFind.hs
+++ MelFind.hs
@@ -2,7 +2,7 @@ module MelFind where -- ( findMelody ) where
 
 import ZMidiBasic
 
-import Data.List          ( sortBy, groupBy )
+import Data.List          ( sortBy, groupBy, partition )
 import Data.Function      ( on )
 import Data.Ord           ( comparing )
 import Control.Arrow      ( (***), second )
@@ -11,6 +11,8 @@ import Control.Arrow      ( (***), second )
 --------------------------------------------------------------------------------
 -- Melody Finding
 --------------------------------------------------------------------------------
+
+type MelFind = Voice -> (Voice, Voice)
 
 -- | Applies 'findMelodyQuant' to a 'MidiScore' but returns the score instead
 -- of just the 'Voice'.
@@ -22,7 +24,8 @@ filterMelodyQuant q ms = ms {getVoices = [findMelodyQuant q ms]}
 -- 'FourtyEighth' and the onverlapping notes are cut off. [more..?]
 findMelodyQuant :: ShortestNote ->  MidiScore -> Voice
 findMelodyQuant q = removeOverlap . head . getVoices 
-                                  . sepHand (skyLineLowLim (Pitch (0,0))) 
+                                  -- . sepHand (skyLineLowLim (Pitch (0,0))) 
+                                  . sepHand skyLineLLDipDetect
                                   . quantise q . mergeTracks 
 
 -- | Returns the melody 'Voice', if there the 'MidiScore' has exactly 2 voices
@@ -34,19 +37,23 @@ getMelody ms = case getVoices ms of
 -- | Merges all tracks into one track
 mergeTracks :: MidiScore -> MidiScore
 mergeTracks ms = 
-  ms {getVoices = [sortBy (comparing onset) . setChans 0 . concat . getVoices $ ms]} 
+  ms {getVoices = [sortVoice . setChans 0 . concat . getVoices $ ms]} 
  
 -- TODO: adapt channel number
 -- | A hand separation function that takes a separation function and applies
 -- this to a 'MidiScore'. sepHand throws an error the number of tracks is not 1
-sepHand :: (Voice -> (Voice, Voice)) -> MidiScore -> MidiScore
+sepHand :: MelFind -> MidiScore -> MidiScore
 sepHand f mf = case getVoices mf of
   [x] -> let (r,l) = f x in mf {getVoices = [r,l]}
   _   -> error "sepHand: more or less than 1 voice!"
 
+
+skyLineLLDipDetect :: Voice -> (Voice, Voice)
+skyLineLLDipDetect = combine (skyLineLowLim (Pitch (0,0))) (dipDetect (-9) 9)
+  
 -- | An implementation of the "skyline algorithm" that picks the highest note  
 -- in a melody (high, low)
-skyLine :: Voice -> (Voice, Voice)    
+skyLine :: Voice -> (Voice, Voice)  -- MelFind
 skyLine = skyLineLowLim (Pitch (-5,0))
     
 -- | An implementation of the "skyline algorithm" that picks the highest note
@@ -65,21 +72,33 @@ pickHigh p l | getPitch h >= p = ([h], t)
              | otherwise       = ([ ], l) 
                  where (h:t)   = reverse . sortBy (comparing getPitch) $ l 
 
+
+--------------------------------------------------------------------------------
+-- Dip detection
+--------------------------------------------------------------------------------
+
+dipDetect :: Interval -> Interval -> Voice -> (Voice, Voice)
+dipDetect dwn up v = 
+  (f *** f) . partition ((Dip /= ) . snd) . markDip dwn up $ v where f = map fst
        
-data IntClass = Up | Down | OK | Drop deriving (Eq, Show)
+data IntClass = Up | Down | OK | Dip deriving (Eq, Show)
 
-dipDetect :: Interval -> Interval -> [Timed ScoreEvent] 
-          -> [(Timed ScoreEvent, IntClass)]
-dipDetect dwn up v = foldr mark [] . foldr classInter [] $ v where
+-- | Marks notes of they represent a significant dip
+markDip :: Interval -> Interval -> [Timed ScoreEvent] 
+        -> [(Timed ScoreEvent, IntClass)]
+markDip dwn up v = foldr mark [] . foldr classInter [] $ v where
 
+  -- Given a note and its interval classes, dips are marked
   mark :: (Timed ScoreEvent, IntClass) -> [(Timed ScoreEvent , IntClass)]
          -> [(Timed ScoreEvent, IntClass)]
   mark x []    = [x]
   mark s (h:t) = case (snd s, snd h) of
-    (Down, Up) -> s : second (const Drop) h : t
-    _          -> s : h                     : t
+    (Down, Up) -> s : second (const Dip) h : t -- mark Down-Up patterns
+    _          -> s : h                    : t -- otherwise continue
 
-  classInter :: Timed ScoreEvent -> [(Timed ScoreEvent , IntClass)]
+  -- classifies an interval between to notes. A note is coupled with the 
+  -- interval class between the previous note and the note itself
+  classInter :: Timed ScoreEvent -> [(Timed ScoreEvent, IntClass)]
            -> [(Timed ScoreEvent, IntClass)]
   classInter a []            = [(a, OK  )]
   classInter a l | i >= up   =  (a, Up  ) : l
@@ -90,6 +109,15 @@ dipDetect dwn up v = foldr mark [] . foldr classInter [] $ v where
 --------------------------------------------------------------------------------
 -- Utilities
 --------------------------------------------------------------------------------
+
+combine :: MelFind -> MelFind -> (Voice -> (Voice, Voice))
+combine f s v = let (melf, accf) = f v
+                    (mels, accs) = s melf
+                in  (mels, sortVoice (accs ++ accf))
+
+-- | Sorts a 'Voice' based on the onset times of the 'ScoreEvents'
+sortVoice :: Voice -> Voice 
+sortVoice = sortBy (comparing onset)
 
 setChans :: Channel -> [Timed ScoreEvent] -> [Timed ScoreEvent]
 setChans c = map (setChan c)
