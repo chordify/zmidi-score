5a0e69a253fe6d61e793f94ae7bddebc399b33c2
Bas de Haas <W.B.deHaas@uu.nl>
2013-10-15 15:00:03 +0200

    turned type into newtype, improved printing

--- LocalMeter.hs
+++ LocalMeter.hs
@@ -1,8 +1,10 @@
-{-# OPTIONS_GHC -Wall #-}
+{-# OPTIONS_GHC -Wall                   #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module LocalMeter where
 
+import Data.List                  ( intercalate )
 import Data.IntMap                ( IntMap, insertWith, mapWithKey 
-                                  , split, fromList, showTree )
+                                  , split, fromList, showTree, foldrWithKey )
 import qualified Data.IntMap as M ( foldr )
 
 test :: MeterMap
@@ -10,87 +12,100 @@ test = fromList [(1, [(1,2), (2,4)]), (3, [(1,4), (2,2)]), (4, [])]
 
 -- | A matrix is a Vector of Vectors, we could also use one large Vector with
 -- another 'ix' function
-type LMeters = [(Period, [(Time, Length)])]
+type LMeters = [(Period, [(Time, Len)])]
 
--- type SMeters = [(Time, [(Time, [(Period, Length)])])]
--- type SMeters = IntMap (IntMap [(Period, Length)])
-type MeterMap = IntMap [(Period, Length)]
+-- type SMeters = [(Time, [(Time, [(Period, Len)])])]
+-- type SMeters = IntMap (IntMap [(Period, Len)])
+type MeterMap = IntMap [(Period, Len)]
 
-insertMeter :: MeterMap -> Period -> (Time, Length) -> MeterMap
-insertMeter m p (s,l) = insertWith (++) s [(p,l)] m
+insertMeter :: MeterMap -> Period -> (Time, Len) -> MeterMap
+insertMeter m p (s,l) = insertWith (++) (time s) [(p,l)] m
 
 -- filter all local meters with an onset greater then o, and which are in reach 
 -- of o
 filterLMeters :: Time -> MeterMap -> MeterMap
-filterLMeters o = mapWithKey filterInReach . fst . split (succ o) 
+filterLMeters o = mapWithKey filterInReach . fst . split (succ . time $ o) 
 
-  where filterInReach :: Time -> [(Period, Length)] -> [(Period, Length)]
-        filterInReach s = filter (\x -> inReach s x && matchesPhase s x)
+  where filterInReach :: Int -> [(Period, Len)] -> [(Period, Len)]
+        filterInReach s = filter (\x -> inReach      (Time s) x 
+                                     && matchesPhase (Time s) x)
         
         -- returns true if  onset o lies in reach the meter starting at s
-        -- with period p and length l
-        inReach :: Time -> (Period, Length) -> Bool
+        -- with period p and Len l
+        inReach :: Time -> (Period, Len) -> Bool
         inReach s (p, l) = o <= meterEnd p l s
         
         -- given an onset and an 'LMeter' returns True if both have the same 
         -- phase, which means that the onset coincides with the grid of the 'LMeter'
         -- matchesPhase :: Time -> LMeter -> Bool
         -- matchesPhase o (LMeter strt per _len) = (o - strt) `mod` per == 0 
-        matchesPhase :: Time -> (Period, Length) -> Bool
-        matchesPhase s (p, _ ) = (o - s) `mod` p == 0
+        matchesPhase :: Time -> (Period, Len) -> Bool
+        matchesPhase (Time s) (Period p, _ ) = (time o - s) `mod` p ==  0
 
 nrOfLMeters :: MeterMap -> Int
 nrOfLMeters = M.foldr step 0 where
   
-  step :: [(Period, Length)] -> Int -> Int
+  step :: [(Period, Len)] -> Int -> Int
   step l r = r + length l
 
+toLMeters :: MeterMap -> [LMeter]
+toLMeters = foldrWithKey step [] where
 
+  step :: Int -> [(Period, Len)] -> [LMeter] -> [LMeter]
+  step s x [] =       map (toLMeter s) x 
+  step s x ms = ms ++ map (toLMeter s) x 
+  
+  toLMeter :: Int -> (Period, Len) -> LMeter
+  toLMeter s (p, l) = LMeter (Time s) p l
 
-data LMeter = LMeter { start   :: Time
-                     , period  :: Period
-                     , mlength :: Length } deriving (Eq)
+data LMeter = LMeter { mStart  :: Time
+                     , mPeriod :: Period
+                     , mLen    :: Len } deriving (Eq)
 
 instance Show LMeter where
-  show (LMeter s p l) = '(' : show s ++ ", " ++ show p ++ ", " ++ show l ++ ")"
+  show (LMeter s p l) = "(O: "  ++ show (time s) ++ 
+                        ", P: " ++ show (period p) ++ 
+                        ", L: " ++ show (len l) ++ ")"
 
-type Length = Int
-type Period = Int
-type Time   = Int
-type Weight = Int
+  showList a b = (intercalate "\n" . map show $ a) ++ b
+
+newtype Len    = Len    { len    :: Int } deriving ( Eq, Show, Num, Ord, Enum )
+newtype Period = Period { period :: Int } deriving ( Eq, Show, Num, Ord, Enum )
+newtype Time   = Time   { time   :: Int } deriving ( Eq, Show, Num, Ord, Enum )
+-- newtype Weight = Int
 
                      
 -- | Indexing a matrix
-getStarts :: [(Time,Length)] -> Length -> [Time] 
+getStarts :: [(Time,Len)] -> Len -> [Time] 
 getStarts m l = map fst $ filter ((== l) . snd) m 
 
-getLengths :: [(Time,Length)] -> Time -> [Length] 
-getLengths m s = map snd $ filter ((== s) . fst) m
+getLens :: [(Time,Len)] -> Time -> [Len] 
+getLens m s = map snd $ filter ((== s) . fst) m
 
--- getLMeters :: LMeters -> Period -> Length -> [LMeter]
+-- getLMeters :: LMeters -> Period -> Len -> [LMeter]
 -- getLMeters m p l = map (\(o,_) -> LMeter o p l) $ filter ((== l) . snd) (m !! p) 
 
-addLMeter :: [(Time, Length)] -> Period -> Time -> Length -> [(Time,Length)]
+addLMeter :: [(Time, Len)] -> Period -> Time -> Len -> [(Time,Len)]
 addLMeter m p t l | isMaximal (t,l) = addMeter (t,l)
                   | otherwise       = m
 
   where -- adds a meter to the collection, and removes any meters that are 
         -- included in the added meter
-        addMeter :: (Time, Length) -> [(Time, Length)]
+        addMeter :: (Time, Len) -> [(Time, Len)]
         addMeter x = x : filter (\y -> not $ isSubSet y x) m
         
         -- being maximal means not being a subset
-        isMaximal :: (Time, Length) -> Bool
+        isMaximal :: (Time, Len) -> Bool
         isMaximal x = and $ map (not . isSubSet x) m
         
         -- returns true if the first pair is a meter that is a subset of the
         -- second meter pair
-        isSubSet :: (Time, Length) -> (Time, Length) -> Bool
+        isSubSet :: (Time, Len) -> (Time, Len) -> Bool
         isSubSet (ta, la) (tb, lb) =             ta    >=            tb 
-                                   && meterEnd p ta la <= meterEnd p tb lb
+                                   && meterEnd p la ta <= meterEnd p lb tb
                                            
-meterEnd :: Period -> Length -> Time -> Time
-meterEnd p len tm = tm + (p * len)
+meterEnd :: Period -> Len -> Time -> Time
+meterEnd (Period p) (Len l) (Time t) = Time (t + (p * l))
         
 
 
