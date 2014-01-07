2d0cbbc247b0bd69bbed253926bd35131e621d61
Bas de Haas <W.B.deHaas@uu.nl>
2014-01-07 17:27:03 +0100

    added a few datatypes to make the code more type safe

--- RagPatIMA.hs
+++ RagPatIMA.hs
@@ -1,4 +1,5 @@
-{-# OPTIONS_GHC -Wall #-}
+{-# OPTIONS_GHC -Wall                   #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Main where
 
 import ZMidiBasic            
@@ -12,11 +13,16 @@ import System.Environment           ( getArgs )
 import Data.List            ( nubBy )
 import Data.Function                ( on )
 
-type IMAMatch = Float
-type Pattern  = (Int, Float)
+-- type Pattern  = (Int, Float)
 
-matchMeterIMA :: ShortestNote -> MidiScore 
-              -> [TimedSeg TimeSig [Timed (Maybe ScoreEvent, Double)]]
+-- | Normalised spectral weights (value between 0 and 1)
+newtype NSWeigth = NSWeigth { nsweight :: Double }
+                     deriving ( Eq, Show, Num, Ord, Enum, Real, Floating
+                              , Fractional, RealFloat, RealFrac )
+                              
+type NSWMeterSeg = TimedSeg TimeSig [Timed (Maybe ScoreEvent, NSWeigth)]
+
+matchMeterIMA :: ShortestNote -> MidiScore -> [NSWMeterSeg]
 matchMeterIMA q ms = 
   let -- quantise the merge all tracks and remove nub notes with the same onset
       v   = nubBy ((==) `on` onset) 
@@ -26,13 +32,13 @@ matchMeterIMA q ms =
       -- calculate the spectral weights
       ws  = getSpectralWeight mn . map IMA.Time . toOnsets $ v
       -- calculate the maximum weight
-      mx  = fromIntegral . maximum . map snd $ ws
+      mx  = NSWeigth . fromIntegral . maximum . map snd $ ws
       -- split the midi file per 
   in map normaliseTime $ segment (getTimeSig ms) (match mx ws v) 
 
 -- | matches a grid with spectral weights with the onsets that created the
 -- weights. The first argument is the maximum 'Weight' found among the weights
-match :: Double -> [(Int, SWeight)] -> Voice -> [Timed (Maybe ScoreEvent, Double)]
+match :: NSWeigth -> [(Int, SWeight)] -> Voice -> [Timed (Maybe ScoreEvent, NSWeigth)]
 match _ [] []              = []
 match m ((g, w):ws) []     =          addWeight m w (Left g) : match m ws []
 match m ((g, w):ws) (t:ts) | g <  o = addWeight m w (Left g) : match m ws (t:ts)
@@ -41,8 +47,8 @@ match m ((g, w):ws) (t:ts) | g <  o = addWeight m w (Left g) : match m ws (t:ts)
                                where o = onset t
 match _ _ _                = error "list of unequal lengths"             
 
-addWeight :: Double -> SWeight -> Either Int (Timed ScoreEvent) 
-          -> Timed (Maybe ScoreEvent, Double)
+addWeight :: NSWeigth -> SWeight -> Either Int (Timed ScoreEvent) 
+          -> Timed (Maybe ScoreEvent, NSWeigth)
 addWeight m w e = either ((flip Timed) (Nothing, w')) f e
   where w'  = fromIntegral w / m
         f t = t {getEvent = (Just $ getEvent t, w')}
@@ -84,19 +90,28 @@ testBeatBar fp = do ms <- readMidiScore fp
 -}
 
 --
-starMeter :: Time -> (TimedSeg TimeSig [Timed (Maybe ScoreEvent, Double)]) -> IO ()
+starMeter :: Time -> NSWMeterSeg -> IO ()
 starMeter tpb (TimedSeg (Timed t ts) s) = 
   do putStrLn (show t ++ " ================== " ++ show ts ++ " ==================" )
      mapM_ (toStar t ts) s where
                     
   -- prints one line e.g. "1152 1 3 1C  ***************"
-  toStar :: Time -> TimeSig -> Timed (Maybe ScoreEvent, Double) -> IO ()
+  toStar :: Time -> TimeSig -> Timed (Maybe ScoreEvent, NSWeigth) -> IO ()
   toStar os x (Timed g (se,w)) = 
     let (bar, bt) = getBeatInBar x tpb g
     in  putStrLn (show (g+os) ++ " " ++ show bar ++ " " ++ (maybe " "  show bt)
                               ++ " " ++ (maybe "   " (show . pitch) se)
                               ++ " " ++ replicate (round (20 * w)) '*' )
 
+--------------------------------------------------------------------------------
+-- IMA profiles
+--------------------------------------------------------------------------------
+
+data NSWProf = NSWProf { pTimeSig :: TimeSig 
+                       , pWeights :: [SWeight]
+                       } deriving (Show, Eq)
+                       
+                       
 -- testing
 main :: IO ()
 main = do arg <- getArgs 
