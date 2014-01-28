b50f736f72349a18a47a3633b1a3d14b9f3b4a7d
Bas de Haas <W.B.deHaas@uu.nl>
2014-01-28 15:56:20 +0100

    promoted some functions

--- src/ZMidi/Score/ToMidiScore.hs
+++ src/ZMidi/Score/ToMidiScore.hs
@@ -72,77 +72,77 @@ midiTrackToVoice m =
                                     (VoiceEvent _ _) -> voiceEvent mm
                                     (MetaEvent  _)   -> metaEvent  mm
                                     _                -> return Nothing
-                            
-    -- Transforms a 'MidiMessage' containing a 'VoiceEvent' into a 'ScoreEvent'
-    voiceEvent :: MidiMessage -> State MidiState (Maybe (Timed ScoreEvent))
-    voiceEvent mm = case getVoiceEvent mm of
-      Just   (NoteOff  chn  ptch _vel) -> toMidiNote chn ptch
-      Just   (NoteOn   chn  ptch 0   ) -> toMidiNote chn ptch
-      Just n@(NoteOn  _chn _ptch _vel)
-         -> do  t <- gets fst
-                -- replace the deltaTime in the noteOn event by the absolute
-                -- time as stored in the MidiState
-                modify . addMessage $ (t, n)
-                return Nothing
-      _  ->     return Nothing    -- ignore NoteAftertouch, Controller,
-                                  -- ProgramChange, ChanAftertouch, PitchBend
-    
-    -- Transforms a 'MidiMessage' containing a 'MetaEvent' into a 'ScoreEvent'
-    metaEvent :: MidiMessage -> State MidiState (Maybe (Timed ScoreEvent))
-    metaEvent mm = 
-      do t <- gets fst
-         case getMetaEvent mm of
-            -- tempo meta event
-            Just (SetTempo tp)  
-              -> return . Just . Timed t . TempoChange . fromIntegral $ tp
-            -- timesignature meta event
-            Just (TimeSignature num den metr n32n) 
-              -> return . Just . Timed t $ TimeSigChange 
-                                (TimeSig (fromIntegral num) (2 ^ den) metr n32n)
-            -- key meta event
-            Just (KeySignature root scale)
-              -> return . Just $ Timed t (KeyChange (Key root scale ))
-            _ -> return Nothing
-    
-    -- Some utilities:
-    -- Given a note-on and a note off event, creates a new MidiNote ScoreEvent
-    toMidiNote :: Word8 -> Word8 -> State MidiState (Maybe (Timed ScoreEvent))
-    toMidiNote c p = 
-      do ms <- gets snd
-         case span (not . isNoteOnMatch c p) ms of
-           -- TODO: perhaps we should store the missed note offs??
-           (_, []               ) ->  -- trace ("no note on found for: " ++ show p
-                                      --      ++  " on channel: "      ++ show c)
-                                           (return Nothing)
-           (x, (ons, noteOn) : y) -> 
-              do modify (setMessages (x ++ y))
-                 t  <- gets fst
-                 -- N.B. the delta time in the note on has been replace 
-                 -- by an absolute timestamp
-                 return . Just . Timed ons $ NoteEvent (fromIntegral c) 
-                                   (toPitch p) (getVelocity noteOn) (t - ons)
+                        
+-- Transforms a 'MidiMessage' containing a 'VoiceEvent' into a 'ScoreEvent'
+voiceEvent :: MidiMessage -> State MidiState (Maybe (Timed ScoreEvent))
+voiceEvent mm = case getVoiceEvent mm of
+  Just   (NoteOff  chn  ptch _vel) -> toMidiNote chn ptch
+  Just   (NoteOn   chn  ptch 0   ) -> toMidiNote chn ptch
+  Just n@(NoteOn  _chn _ptch _vel)
+     -> do  t <- gets fst
+            -- replace the deltaTime in the noteOn event by the absolute
+            -- time as stored in the MidiState
+            modify . addMessage $ (t, n)
+            return Nothing
+  _  ->     return Nothing    -- ignore NoteAftertouch, Controller,
+                              -- ProgramChange, ChanAftertouch, PitchBend
+
+-- Transforms a 'MidiMessage' containing a 'MetaEvent' into a 'ScoreEvent'
+metaEvent :: MidiMessage -> State MidiState (Maybe (Timed ScoreEvent))
+metaEvent mm = 
+  do t <- gets fst
+     case getMetaEvent mm of
+        -- tempo meta event
+        Just (SetTempo tp)  
+          -> return . Just . Timed t . TempoChange . fromIntegral $ tp
+        -- timesignature meta event
+        Just (TimeSignature num den metr n32n) 
+          -> return . Just . Timed t $ TimeSigChange 
+                            (TimeSig (fromIntegral num) (2 ^ den) metr n32n)
+        -- key meta event
+        Just (KeySignature root scale)
+          -> return . Just $ Timed t (KeyChange (Key root scale ))
+        _ -> return Nothing
+
+-- Some utilities:
+-- Given a note-on and a note off event, creates a new MidiNote ScoreEvent
+toMidiNote :: Word8 -> Word8 -> State MidiState (Maybe (Timed ScoreEvent))
+toMidiNote c p = 
+  do ms <- gets snd
+     case span (not . isNoteOnMatch c p) ms of
+       -- TODO: perhaps we should store the missed note offs??
+       (_, []               ) ->  -- trace ("no note on found for: " ++ show p
+                                  --      ++  " on channel: "      ++ show c)
+                                       (return Nothing)
+       (x, (ons, noteOn) : y) -> 
+          do modify (setMessages (x ++ y))
+             t  <- gets fst
+             -- N.B. the delta time in the note on has been replace 
+             -- by an absolute timestamp
+             return . Just . Timed ons $ NoteEvent (fromIntegral c) 
+                               (toPitch p) (getVelocity noteOn) (t - ons)
             
-    -- returns True if the NoteOn MidiMessage maches a Channel and Pitch
-    isNoteOnMatch :: Word8 -> Word8 -> (Time, MidiVoiceEvent) -> Bool
-    isNoteOnMatch offc offp (_t, NoteOn onc onp _v) = onc == offc && onp == offp
-    isNoteOnMatch _c   _p    _                      = False
+-- returns True if the NoteOn MidiMessage maches a Channel and Pitch
+isNoteOnMatch :: Word8 -> Word8 -> (Time, MidiVoiceEvent) -> Bool
+isNoteOnMatch offc offp (_t, NoteOn onc onp _v) = onc == offc && onp == offp
+isNoteOnMatch _c   _p    _                      = False
 
-    -- Given a MidiMessage, returns the MidiVoiceEvent and Nothing if it 
-    -- contains something else. 
-    getVoiceEvent :: MidiMessage -> Maybe MidiVoiceEvent
-    getVoiceEvent (_t, (VoiceEvent _ e)) = Just e
-    getVoiceEvent _                      = Nothing
-      
-    -- Given a MidiMessage, returns the MidiMetaEvent and Nothing if it 
-    -- contains something else.
-    getMetaEvent :: MidiMessage -> Maybe MidiMetaEvent
-    getMetaEvent (_t, (MetaEvent e)) = Just e
-    getMetaEvent _                   = Nothing
+-- Given a MidiMessage, returns the MidiVoiceEvent and Nothing if it 
+-- contains something else. 
+getVoiceEvent :: MidiMessage -> Maybe MidiVoiceEvent
+getVoiceEvent (_t, (VoiceEvent _ e)) = Just e
+getVoiceEvent _                      = Nothing
+  
+-- Given a MidiMessage, returns the MidiMetaEvent and Nothing if it 
+-- contains something else.
+getMetaEvent :: MidiMessage -> Maybe MidiMetaEvent
+getMetaEvent (_t, (MetaEvent e)) = Just e
+getMetaEvent _                   = Nothing
 
-    getVelocity :: MidiVoiceEvent -> Velocity
-    getVelocity (NoteOn  _ _ v) = Velocity v
-    getVelocity (NoteOff _ _ v) = Velocity v
-    getVelocity _               = error "not a noteOn or a noteOff event"
+getVelocity :: MidiVoiceEvent -> Velocity
+getVelocity (NoteOn  _ _ v) = Velocity v
+getVelocity (NoteOff _ _ v) = Velocity v
+getVelocity _               = error "not a noteOn or a noteOff event"
     
 -- We define a stat to store the absolute midi clock 
 -- and a list of note-on events.
