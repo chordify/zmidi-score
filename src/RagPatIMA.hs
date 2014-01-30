e35436c48c10b481f967890cdc5ae2cc2e742d68
Bas de Haas <W.B.deHaas@uu.nl>
2014-01-30 11:57:15 +0100

    added documentation

--- src/RagPatIMA.hs
+++ src/RagPatIMA.hs
@@ -60,7 +60,7 @@ toIMAOnset = map fromIntegral . toOnsets
 matchScore :: Voice -> [(Int, SWeight)] -> [Timed (Maybe ScoreEvent, NSWeight)]
 matchScore v s = match (map (first Time) s) v where
 
-  -- The first argument is the maximum 'Weight' found among the weights
+  -- The maximum 'Weight' found among the weights
   mx = maximum . map snd $ s
   
   -- | matches a grid with spectral weights with the onsets that created the
@@ -78,10 +78,11 @@ matchScore v s = match (map (first Time) s) v where
   addWeight :: SWeight -> Either Time (Timed ScoreEvent) 
             -> Timed (Maybe ScoreEvent, NSWeight)
   addWeight w e = either ((flip Timed) (Nothing, w')) f e
+    
     where w'  = fromIntegral w / fromIntegral mx
           f t = t {getEvent = (Just $ getEvent t, w')}
 
---
+-- 
 starMeter :: Time -> NSWMeterSeg -> IO ()
 starMeter tpb (TimedSeg (Timed t ts) s) = 
   do putStrLn . printf ("%6d: ======================= " ++ show ts 
@@ -150,6 +151,7 @@ mergeNSWProf (a, ma) (b, mb) = let m = unionWith (+) ma mb in m `seq` (a + b, m)
 -- exporting / importing IMA profiles
 --------------------------------------------------------------------------------
 
+-- exports a normalised inner metric analysis profiles to a binary file
 safeNSWProf :: FilePath -> Map TimeSig NSWProf -> IO (Map TimeSig NSWProf)
 safeNSWProf fp m = encodeFile fp m >> putStrLn ("written: " ++ fp) >> return m
   
@@ -180,25 +182,29 @@ main =
                         
        _    -> error "Please use -f <file> or -d <ragtime directory>"
    
+-- combines two inner metrical analysis maps into one, summing all results
 unionNWProfMaps :: [Map TimeSig NSWProf] -> IO (Map TimeSig NSWProf)
 unionNWProfMaps m = do let r = foldr (unionWith mergeNSWProf) empty m
                        r `seq` return r
 
-
+-- Prints the average normalised inner metric analysis profiles to the user
 printMeterStats :: Map TimeSig NSWProf -> IO ()
 printMeterStats = mapM_ (putStrLn . showNSWProf) . toList 
    
+-- Reads a file and does an inner metric analysis per time signature segment
 readProf :: FilePath -> IO (Map TimeSig NSWProf)
 readProf fp = do qm <- readQMidiScoreSafe FourtyEighth fp 
                  case qm >>= qMidiScoreToNSWProfMaps of
                    Right w -> return w
                    Left  e -> warning fp e >> return empty
                  
+-- Transforms quantised midi into an inner metric analysis or a failure warning
 qMidiScoreToNSWProfMaps :: QMidiScore -> Either String (Map TimeSig NSWProf)
 qMidiScoreToNSWProfMaps qms =     timeSigCheck qms 
                               >>= toNSWProfSegs
                               >>= (\x -> return $ collectNSWProf x empty)
        
+-- Checks for a valid time siganture
 timeSigCheck :: QMidiScore -> Either String QMidiScore
 timeSigCheck ms | hasTimeSigs (qMidiScore ms) = Right ms
                 | otherwise = Left "Has no valid time signature"
