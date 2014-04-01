550485ee5cf9dcce3970741a2b1bf1e9dd92922f
Bas de Haas <W.B.deHaas@uu.nl>
2014-04-01 16:17:01 +0200

    we can now batch calculate the new profiles

--- src/EncodeNSWProf.hs
+++ src/EncodeNSWProf.hs
@@ -4,43 +4,57 @@ import qualified Data.ByteString.Lazy as BL
 import Data.Csv
 import qualified Data.Vector as V
 import GHC.Generics
-import ZMidi.Score.Datatypes hiding (numerator, denominator)
-import ZMidi.Score.Quantise (QMidiScore (..), toQBins, QBins, ShortestNote (..))
-import ZMidi.IO.Common       ( readQMidiScoreSafe, mapDir, warning)
+import ZMidi.Score.Datatypes hiding  ( numerator, denominator )
+import ZMidi.Score.Quantise          ( QMidiScore (..), toQBins, QBins, ShortestNote (..))
+import ZMidi.IO.Common               ( readQMidiScoreSafe, mapDir, warning)
 import Ragtime.NSWProf
-import Ragtime.MidiIMA (doIMA, toNSWProfWithTS, fourBarFilter)
-import Ragtime.TimeSigSeg (TimedSeg (..))
-import Ragtime.SelectQBins (selectQBins, filterByQBinStrength, printMeterStats)
-import IMA.InnerMetricalAnalysis (SWeight (..))
-import System.Environment    ( getArgs )
-import Data.List             (intercalate)
-import Data.Ratio            (numerator, denominator)
+import Ragtime.MidiIMA               ( doIMA, toNSWProfWithTS, fourBarFilter)
+import Ragtime.TimeSigSeg            ( TimedSeg (..))
+import Ragtime.SelectQBins           ( selectQBins, filterByQBinStrength, printMeterStats, filterToList )
+import IMA.InnerMetricalAnalysis     ( SWeight (..))
+import System.Environment            ( getArgs )
+import Data.List                     ( intercalate)
+import Data.Ratio                    ( numerator, denominator)
+import Data.Map.Strict               ( Map, elems )
 
-data NSWProfCSV = NSWProfCSV TimeSig (V.Vector NSWeight)
+data NSWProfCSV = NSWProfCSV TimeSig [NSWeight]
 
 instance ToField    NSWeight where toField (NSWeight w) = toField w
 instance ToField    TimeSig  where 
   toField (TimeSig n d _ _) = toField (show n ++ "/" ++ show d)
-
+  
 instance ToRecord   NSWProfCSV where
-  toRecord (NSWProfCSV ts p) = record (toField ts : map toField (V.toList p))
+  toRecord (NSWProfCSV ts p) = record (toField ts : map toField p)
 
-toCSV :: QMidiScore -> Either String [NSWProfCSV]
-toCSV qm = doIMA qm >>= fourBarFilter tb >>= return . map toProf where
+-- instance ToNamedRecord NSWProfCSV where
+  -- toNamedRecord (NSWProfCSV ts p) = 
+    -- namedRecord ((toField "meter" .= ts) : map (\(k,v) -> printKey k .= v) p)  
+  
+printKey :: (Beat, BeatRat) -> Field
+printKey (Beat b, BeatRat br) = toField (show b ++ "." 
+                                      ++ show (numerator br) ++ "." 
+                                      ++ show (denominator br))
+  
+  
+toCSV :: Map TimeSig [(Beat, BeatRat)] -> QMidiScore 
+      -> Either String [NSWProfCSV]
+toCSV s qm = doIMA qm >>= fourBarFilter tb >>= return . map toProf where
 
   tb = ticksPerBeat . qMidiScore $ qm
   qb = toQBins FourtyEighth
 
   toProf :: TimedSeg TimeSig [Timed (Maybe ScoreEvent, SWeight)] -> NSWProfCSV
   toProf (TimedSeg (Timed _ ts) td) = 
-    NSWProfCSV ts . vectorize qb ts . normSWProf . toNSWProfWithTS ts tb $ td
+    NSWProfCSV ts . filterToList s ts              -- only keep the strong pos
+                  . normSWProf     -- normalise the profile 
+                  . toNSWProfWithTS ts tb $ td     -- calculate the profile
     
-processMidi :: FilePath -> IO ()
-processMidi fp = do qm <- readQMidiScoreSafe FourtyEighth fp 
-                    case qm >>= timeSigCheck >>= toCSV of
-                      Left  err -> warning fp err
-                      Right csv -> BL.appendFile "train.csv" . encode $ csv 
-
+processMidi :: Map TimeSig [(Beat, BeatRat)] -> FilePath -> IO ()
+processMidi s fp = do qm <- readQMidiScoreSafe FourtyEighth fp 
+                      case qm >>= timeSigCheck >>= toCSV s of
+                        Left  err -> warning fp err
+                        Right csv -> BL.appendFile "train.csv" . encode $ csv 
+{-
 genHeader :: Int -> String
 genHeader n = intercalate "," $ ("meter" : map toName [0..n]) where
 
@@ -53,14 +67,16 @@ genHeader n = intercalate "," $ ("meter" : map toName [0..n]) where
                                     ++ show (numerator br) ++ "." 
                                     ++ show (denominator br)
               _  -> error ("index out of bounds: " ++ show i)                      
-    
+    -}
 -- testing
 main :: IO ()
 main = 
   do arg <- getArgs 
+     m   <- readNSWProf "ragtimeMeterProfilesTrain_2014-03-25.bin"  
+            >>= return . selectQBins 12
      case arg of
-       ["-f", fp] -> processMidi fp
-       ["-d", fp] -> mapDir processMidi fp >> return ()
+       ["-f", fp] -> processMidi m fp
+       ["-d", fp] -> mapDir (processMidi m) fp >> return ()
        ["-m", fp] -> readNSWProf fp >>= printMeterStats . filterByQBinStrength
        _ -> error "usage: -f <filename> -d <directory>"
 
--- src/Ragtime/SelectQBins.hs
+++ src/Ragtime/SelectQBins.hs
@@ -1,7 +1,9 @@
 {-# OPTIONS_GHC -Wall                   #-}
 module Ragtime.SelectQBins ( selectQBins 
                            , filterByQBinStrength
-                           , filterByQBinStrengthWith
+                           -- , filterByQBinStrengthWith
+                           -- , filterBin
+                           , filterToList
                            , printMeterStats
                            ) where
 
@@ -12,9 +14,12 @@ import Data.Ord                       ( comparing, Down (..) )
 import Data.Maybe                     ( fromJust )
 import qualified Data.Map.Strict as M ( map, lookup )
 import Data.Map.Strict                ( Map, toAscList, filterWithKey
-                                      , mapWithKey )
-                                      
-selectQBins :: Int -> Map TimeSig NSWProf -> Map TimeSig [(Beat, BeatRat)]
+                                      , mapWithKey, findWithDefault )
+import Control.Arrow                  ( second )
+                             
+type QBinSelection = Map TimeSig [(Beat, BeatRat)]
+                             
+selectQBins :: Int -> Map TimeSig NSWProf -> QBinSelection
 selectQBins bs = M.map select where
 
   select :: NSWProf -> [(Beat, BeatRat)]
@@ -22,23 +27,26 @@ selectQBins bs = M.map select where
          . sortBy (comparing (Down . snd)) . toAscList-- sort by weight
          . snd . nswprof                     -- ignore the nr of bars
          
-filterByQBinStrengthWith :: Map TimeSig [(Beat, BeatRat)] -> Map TimeSig NSWProf
-                         -> Map TimeSig NSWProf
-filterByQBinStrengthWith s m = mapWithKey filter m where
+filterByQBinStrengthWith :: QBinSelection -> Map TimeSig NSWProf -> Map TimeSig NSWProf
+filterByQBinStrengthWith s m = mapWithKey (filterBin s) m where
 
-  filter :: TimeSig -> NSWProf -> NSWProf
-  filter ts (NSWProf (n, x)) = 
-    let l = getBins ts
-    in NSWProf (n, filterWithKey (\k _ -> k `elem` l) x)
-  
-  getBins :: TimeSig -> [(Beat, BeatRat)]
-  getBins ts = fromJust $ M.lookup ts s 
+filterBin :: QBinSelection -> TimeSig -> NSWProf -> NSWProf 
+filterBin s ts = NSWProf . second (filterWithKey (\k _ -> k `elem` l)) . nswprof
+
+  where l = fromJust $ M.lookup ts s 
   
 filterByQBinStrength :: Map TimeSig NSWProf -> Map TimeSig NSWProf
 filterByQBinStrength m = filterByQBinStrengthWith (selectQBins 12 m) m
+
+filterToList :: QBinSelection -> TimeSig -> NSWProf -> [NSWeight]
+filterToList s ts (NSWProf (_,p)) = map fnd . fromJust . M.lookup ts $ s
+  
+  where fnd :: (Beat, BeatRat) -> NSWeight
+        fnd k = findWithDefault (NSWeight 0) k p
   
 -- N.B. copied from 
 -- Prints the average normalised inner metric analysis profiles to the user
 printMeterStats :: Map TimeSig NSWProf -> IO ()
 printMeterStats = mapM_ (putStrLn . showNSWProf) . toAscList 
+
 
