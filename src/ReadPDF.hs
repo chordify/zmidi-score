3dcf2429f47d660ad44c30e5b59f8851b936be30
Bas de Haas <W.B.deHaas@uu.nl>
2014-04-08 17:08:23 +0200

    added a PDF of which the specifications can be read in with jSON

--- src/ReadPDF.hs
+++ src/ReadPDF.hs
@@ -3,19 +3,38 @@
 {-# LANGUAGE DeriveGeneric      #-}
 import Data.Aeson                           ( ToJSON (..), FromJSON (..), decode )
 import GHC.Generics                         ( Generic )
+import Data.Vector                          ( Vector )
+import qualified Data.Vector          as V  ( length, head, fromList )
+import Data.Matrix                          
 -- import ZMidi.Score.Datatypes   (TimeSig (..))
 import qualified Data.ByteString.Lazy as BL ( readFile )
 
 
-data ToPDF = ToPDF { -- timeSig  :: String
-                     mu        :: [Double]
+data ToPDF = ToPDF { mu        :: Vector Double
                    , sigma     :: [[Double]]
                    , inv_sigma :: [[Double]]
+                   , det       :: Vector Double -- Singleton
                    , meter     :: (Int,Int)
                    } deriving (Show, Generic)
 
 instance ToJSON ToPDF
 instance FromJSON ToPDF               
                    
+-- | The type of probablility density functions
+type PDF a = a -> Double
+                   
+-- | multivariate normal
+multiNormal :: ToPDF -> PDF (Vector Double)
+multiNormal tpdf xv = 
+  let k        = fromIntegral . V.length . mu $ tpdf
+      m        = colVector . mu $ tpdf
+      x        = colVector xv
+      invSigma = fromLists . inv_sigma $ tpdf
+      detSigma = V.head . det $ tpdf
+      mat1 = 
+  in (recip ((2*pi)**(k/2) * sqrt(detSigma))) 
+    * exp (getElem 1 1 . negate . scaleMatrix 0.5 
+    $ (transpose $ x-m) * invSigma * (x-m) ) 
+    
 -- read the data
 -- BL.readFile "out4.fit.jsonlite.json" >>= return . decode :: IO (Maybe [ToPDF])
