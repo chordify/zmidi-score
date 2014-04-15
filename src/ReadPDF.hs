9c239737fc241ca3e8c175354bdbbd417189e925
Bas de Haas <W.B.deHaas@uu.nl>
2014-04-15 16:47:44 +0200

    first form of matching implemented

--- src/ReadPDF.hs
+++ src/ReadPDF.hs
@@ -4,40 +4,43 @@
 module ReadPDF where
 import Data.Aeson                           ( ToJSON (..), FromJSON (..), decode )
 import GHC.Generics                         ( Generic )
-import Data.Vector                          ( Vector )
-import qualified Data.Vector          as V  ( length, head, fromList )
+-- import Data.Vector                          ( Vector )
+-- import qualified Data.Vector          as V  ( length, head, fromList )
 import Data.Matrix                          
 import ZMidi.Score.Datatypes                (TimeSig (..))
 -- import Data.Function                        ( on )
-import Data.Ord                             ( comparing, Down (..))
-import Data.List                            ( sortBy )
+-- import Data.Ord                             ( comparing, Down (..))
+-- import Data.List                            ( sortBy )
 import qualified Data.ByteString.Lazy as BL ( readFile )
 
-
-data ToPDF = ToPDF { mu        :: Vector Double
+-- TODO store vector lengths
+data ToPDF = ToPDF { mu        :: [ Double ]
                    , sigma     :: [[Double]]
                    , inv_sigma :: [[Double]]
-                   , prior     :: Vector Double -- Singleton
+                   , prior     :: [ Double ] -- Singleton
                    , meter     :: (Int,Int)
                    } deriving (Show, Generic)
 
 instance ToJSON ToPDF
 instance FromJSON ToPDF               
                    
--- | The type of probablility density functions
+-- | The type of probability density functions
 type PDF a = a -> Double
                    
+-- | TODO Checks whether the sizes of all lists match
+sizeCheck :: ToPDF -> Bool
+sizeCheck = undefined
+                   
 -- | multivariate normal
-multiNormal :: ToPDF -> PDF (Vector Double)
-multiNormal tpdf xv = 
-  let k        = fromIntegral . V.length . mu $ tpdf
-      m        = colVector . mu $ tpdf
-      x        = colVector xv
-      invSigma = fromLists . inv_sigma $ tpdf
-      detSigma = detLU . fromLists . sigma $ tpdf
-  in (recip ((2*pi)**(k/2) * sqrt(detSigma))) 
-    * exp (getElem 1 1 . negate . scaleMatrix 0.5 
-    $ (transpose $ x-m) * invSigma * (x-m) )   
+multiNormal :: ToPDF -> PDF [Double]
+multiNormal tpdf l = 
+  let k        = length . mu $ tpdf -- dimensionality 
+      m        = fromList k 1 . mu $ tpdf          -- column vector of means
+      x        = fromList k 1 l                    -- column vector of l
+      invSigma = fromLists . inv_sigma $ tpdf      -- inverse covariance matrix
+      detSigma = detLU . fromLists . sigma $ tpdf  -- the determinant
+  in (recip ( (2*pi) ** (fromIntegral k /2) * sqrt detSigma ))
+    * exp (getElem 1 1 . scaleMatrix (-0.5) $ (transpose $ x-m) * invSigma * (x-m))   
     
 -- read the data
 readPDFs :: FilePath -> IO [ToPDF]
@@ -47,5 +50,10 @@ readPDFs fp = do d <- BL.readFile fp >>= return . decode
                    _      -> error "There was a type / JSON missmatch"
 
 
+pdfPrior :: ToPDF -> Double
+pdfPrior pdf = case prior pdf of
+                [x] -> x
+                _   -> error "Incorrect JSON: found multiple priors"
+                   
 pdfTimeSig :: ToPDF -> TimeSig
 pdfTimeSig tpdf = let (n,d) = meter tpdf in TimeSig n d 0 0
