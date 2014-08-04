e873bffff1b9bfea37941176bdf6fbcc48ec1903
Bas de Haas <W.B.deHaas@uu.nl>
2014-08-04 17:02:54 +0200

    preparing the groundtruth to be merged with the NSWPStore

--- src/ZMidi/IMA/MeterGT.hs
+++ src/ZMidi/IMA/MeterGT.hs
@@ -1,5 +1,6 @@
 {-# OPTIONS_GHC -Wall          #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveFunctor     #-}
 -- todo move to ZMidi.IO
 module ZMidi.IMA.MeterGT where
 
@@ -8,25 +9,26 @@ import Prelude              hiding ( readFile )
 import ZMidi.Score                 ( TimeSig (..) )
 import ZMidi.IMA.SelectProfBins    ( Rot (..) )
 
-import Data.Vector                 ( Vector )
+import Data.Vector                 ( Vector, toList )
 import Data.Csv                    ( FromField (..), FromNamedRecord (..)
                                    , decodeByName, (.:), Header)
 import Data.List                   ( intercalate )
 import Data.ByteString.Lazy        ( readFile )
 import Control.Applicative         ( (<$>), (<*>), pure )
 import Control.Monad               ( mzero )
+import Control.Arrow               ( second )
 
-data MeterGT = MeterGT { gtFile  :: FilePath
-                       , gtMeter :: TimeSig
-                       , gtRot   :: Rot
-                       , gtNotes :: String
-                       } 
+data MeterGT a = MeterGT { gtFile  :: FilePath
+                         , gtMeter :: a
+                         , gtRot   :: Rot
+                         , gtNotes :: String
+                         } deriving Functor
                        
-instance Show MeterGT where
+instance Show a => Show (MeterGT a) where
   show (MeterGT f ts r s) = intercalate [',', ' '] [f, show ts, show . rot $ r, s]
   showList l s = s ++ (intercalate ['\n'] . map show $ l)
 
-instance FromNamedRecord MeterGT where
+instance FromField a => FromNamedRecord (MeterGT a) where
   parseNamedRecord m = MeterGT <$> m .: "File" 
                                <*> m .: "Corrected"
                                <*> m .: "Rotation"
@@ -41,7 +43,14 @@ instance FromField TimeSig where
                    "'6/8'" -> pure $ TimeSig 6 8 0 0
                    _      -> mzero
                        
-                                              
-readGT :: FilePath -> IO (Either String (Header, Vector MeterGT))
-readGT f = readFile f >>= return . decodeByName 
+mergeMetersOfSong :: [MeterGT TimeSig] -> [MeterGT [TimeSig]]
+mergeMetersOfSong = foldr step [] where
+
+  step :: MeterGT TimeSig -> [MeterGT [TimeSig]] -> [MeterGT [TimeSig]]
+  step m []    = [fmap return m] 
+  step m (h:t) | gtFile m == gtFile h = fmap (gtMeter m :) h : t
+               | otherwise            = fmap return m   :  h : t
+                       
+readGT :: FilePath -> IO (Either String (Header, [MeterGT TimeSig]))
+readGT f = readFile f >>= return . fmap (second toList) . decodeByName 
 
