9c5c308f33c0e20f9eec1a8d92f8a113934b00cf
Bas de Haas <W.B.deHaas@uu.nl>
2014-09-02 17:01:01 +0200

    added conversion from BarRat to (Beat, BarRat)

--- src/ZMidi/Score/BarBeatPos.hs
+++ src/ZMidi/Score/BarBeatPos.hs
@@ -1,4 +1,6 @@
 module ZMidi.Score.BarBeatPos ( getBeatInBar
+                              , getBarRat
+                              , toRatInBeat
                               , toBarRat 
                               ) where
 
@@ -23,20 +25,33 @@ getBeatInBar (TimeSig num _den _ _) t o =
   let (Beat bt, rat) = getRatInBeat t o
       (br, bib)      = (succ *** succ) $ bt `divMod` num 
   in (Bar br, Beat bib, rat)
-
-getBarRat :: TimeSig -> TPB -> Time -> (Bar, BarRat)
-getBarRat NoTimeSig _ _ = error "getBeatInBar applied to noTimeSig"
-getBarRat (TimeSig num den _ _) (TPB t) (Time o) = 
-  let (beat, rest) = o `divMod` t 
-      (bar , bib)  = beat `divMod` num 
-      br           = ((bib * t) + rest) % (den * t) 
-  in (Bar (succ bar), BarRat br)
-  
   
 -- | Returns the position within a 'Bar', see 'getBeatInBar'.
 getRatInBeat :: TPB -> Time -> (Beat, BeatRat)
 getRatInBeat (TPB t) (Time o) = 
   ((Beat) *** (BeatRat . (% t))) (o `divMod` t)
+
+-- | Similar to 'getBeatInBar' we can also describe the musical position as the
+-- combination of a 'Bar' and a 'BarRat'. The latter denotes the ratio within 
+-- a bar, e.g. BarRat (3 % 4) denotes the 4th 'Beat' in the bar.
+getBarRat :: TimeSig -> TPB -> Time -> (Bar, BarRat)
+getBarRat NoTimeSig _ _ = error "getBeatInBar applied to noTimeSig"
+getBarRat (TimeSig num den _ _) (TPB t) (Time o) = 
+  let (bt, rest) = o `divMod` t 
+      (b , bib)  = bt `divMod` num 
+      br         = ((bib * t) + rest) % (den * t) 
+  in (Bar (succ b), BarRat br)
+
+-- toRatInBeat allows us to convert a 'BarRat' into a ('Beat','BeatRat')
+toRatInBeat :: TimeSig -> BarRat -> (Beat, BeatRat)
+toRatInBeat ts br = countBeat (Beat 0, BeatRat . barRat $ br)
+  
+  where oneBeat = BeatRat (1 % tsNum ts) :: BeatRat
+        
+        countBeat :: (Beat, BeatRat) -> (Beat, BeatRat)
+        countBeat (b,x) | rest < 0   = (b,x)
+                         | otherwise = countBeat (succ b, rest)
+                              where rest = x - oneBeat
 
 -- | Musically it is sometimes more intuitive to have a 'BarRat', i.e. the
 -- onset is defined as the ratio within the bar. For example 1%4 denotes them
